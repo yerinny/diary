031242f1db8adf496823c41c2601ae2e5f712586
yerinny <yerinlee90@gmail.com>
2020-05-18 15:26:18 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+//redux
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import rootReducer from './reducers/index';
+
+//create redux store -> reducers -> 'actions -> actionType' | applyMiddleware()
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+//provide the store to react
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+      <App />
+  </Provider>,
   document.getElementById('root')
 );
 
