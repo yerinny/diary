eca0f539d0eb13dfdfbcda92ab3aeda5a61caa3c
yerinny <yerinlee90@gmail.com>
2020-06-01 12:05:06 -0700

    Create Login.js

---
+++ src/components/Login.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render(){
+        return (
+            <div className="container-fluid">
+                <div className="row text-center">
+                    <div className="col-sm-12 jumbotron">
+                        <h1>Login with your favortie <b>Social Network</b> 
+                        </h1>
+                    </div>
+                    <div className="col-sm-6">
+                        <button className="btn btn-danger btn-lg">
+                            Login with Google
+                        </button>
+                    </div>
+
+                    <div className="col-sm-6">
+                        <button className="btn btn-success btn-lg">
+                            Login with Twitter
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login;
