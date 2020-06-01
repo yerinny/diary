c49e4f565658075b136d0f50a363516a75210530
yerinny <yerinlee90@gmail.com>
2020-06-01 15:54:24 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -14,3 +14,7 @@ import * as firebase from 'firebase';
 
 
   export const database = firebase.database().ref('/notes');
+  
+  export const auth = firebase.auth();
+  export const googleProvider = new firebase.auth.GoogleAuthProvider();
+  export const twitterProvider = new firebase.auth.TwitterAuthProvider();
