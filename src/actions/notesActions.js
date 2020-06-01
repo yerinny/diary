023e0ee5b97dffafaa35069c2d2f6fb2326161cf
yerinny <yerinlee90@gmail.com>
2020-06-01 09:16:13 -0700

    Update notesActions.js

--- src/actions/notesActions.js
+++ src/actions/notesActions.js
@@ -19,3 +19,8 @@ export function getNotes(){
 export function saveNote(note){
     return dispatch => database.push(note)
 }
+
+
+export function deleteNote(id){
+    return dispatch => database.child(id).remove();
+}
