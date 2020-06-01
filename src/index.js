394fbd7d9186e88b1d0a9f43839a34b37f759586
yerinny <yerinlee90@gmail.com>
2020-06-01 12:05:10 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,17 +9,44 @@ import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import rootReducer from './reducers/index';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import Login from './components/Login';
 
 //create redux store -> reducers -> 'actions -> actionType' | applyMiddleware()
 //in order for actions to be async we need applyMiddleware
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
+const Header = () => (
+    <nav className="navbar navbar-default">
+        <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                  <span className="icon-bar" />
+                  <span className="icon-bar" />
+                  <span className="icon-bar" />
+              </button>
+
+                <Link className="navbar-brand" to="/">DIARY2020</Link>
+
+            </div>
+        </div>
+    </nav>
+);
+
+
 //provide the store to react
 
 ReactDOM.render(
   <Provider store={store}>
-      <App />
+    <BrowserRouter>
+      <div>
+         <Switch>
+           <Route path="/" component={App} exact={true} />
+           <Route path="/login" component={Login} exact={true} />
+         </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
