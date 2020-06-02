b67ae8d67f51d84695e5e63a74e20ebe87bace34
yerinny <yerinlee90@gmail.com>
2020-06-02 13:02:02 -0700

    Update userAction.js

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,14 +1,24 @@
-import {auth, googleProvider, twitterProvider} from '../firebase';
-import {GET_USER} from '../actionTypes';
+import { auth, googleProvider, twitterProvider } from '../firebase';
+import { GET_USER, USER_STATUS } from '../actionTypes';
 
 export function getUser(){
     return dispatch => {
+        //show loading status before getting user to true
+        dispatch({
+            type: USER_STATUS,
+            payload: true
+        });
         auth.onAuthStateChanged(user => {
             dispatch({
                 type: GET_USER,
                 payload: user
-            })
-        })
+            });
+        //show loading status to false
+            dispatch({
+                type: USER_STATUS,
+                payload: false
+            });
+        });
     }
 }
 
@@ -19,4 +29,9 @@ export function googleLogin(){
 
 export function twitterLogin(){
     return dispatch => auth.signInWithPopup(twitterProvider);
+}
+
+
+export function logout(){
+    return dispatch => auth.signOut();
 }
