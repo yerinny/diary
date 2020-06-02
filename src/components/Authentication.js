97860e43a88c9a2a4f058f971fe0988f2ced1226
yerinny <yerinlee90@gmail.com>
2020-06-02 16:10:08 -0700

    Create Authentication.js

---
+++ src/components/Authentication.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// with withRouter you can get access to the history object's property
+import { withRouter } from 'react-router-dom';
+
+class Authentication extends Component {
+    componentDidUpdate(){
+        //make sure the loading is done then if no user 
+        //then push them to login page
+
+        const { userLoading, user } = this.props;
+        if (userLoading === false && !user ) {
+            this.props.history.push('/login')
+        }
+    }
+
+    render(){
+        const {user, userLoading, children} = this.props;
+        return (userLoading === false && user) ? <div>{children}</div> : null;
+    }
+}
+
+
+function mapStateToProps(state) {
+    return {
+        user: state.user,
+        userLoading: state.loading.user
+    }
+}
+
+export default withRouter(connect(mapStateToProps)(Authentication));
