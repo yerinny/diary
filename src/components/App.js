697ff84256b17e7d9c743f389817ff46f8a019dd
yerinny <yerinlee90@gmail.com>
2020-06-03 16:49:47 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,127 +1,110 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
-import {connect} from 'react-redux'
-import {getNotes, saveNote, deleteNote} from '../actions/notesActions';
+import { connect } from 'react-redux';
+import { getNotes, saveNote, deleteNote } from '../actions/notesActions';
 import NoteCard from './NoteCard';
-import {getUser} from '../actions/userAction';
-
+import { getUser } from '../actions/userAction';
 
 class App extends Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-        title: '',
-        body: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.renderNotes = this.renderNotes.bind(this);
-  }
-
-  //lifecycle
-  // componentDidMount(){
-  //   //getting the function from redux-store as a prop
-  //   this.props.getNotes();
-  //   this.props.getUser();
-  // }
-
- // handle change
-  handleChange(e){
-      this.setState({
-        [e.target.name]: e.target.value
-      });
-  }
-
-// handle submit
-  handleSubmit(e){
-    e.preventDefault();
-    const note = {
-      title: this.state.title,
-      body: this.state.body
+    constructor(props) {
+        super(props);
+        // state
+        this.state = {
+            title: '',
+            body: ''
+        };
+        // bind
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.renderNotes = this.renderNotes.bind(this);
     }
-    this.props.saveNote(note);
-    this.setState({
-      title: '',
-      body: ''
-    })
-  }
 
-//render posts
-  renderNotes(){
-    // console.log(this.props.notes);
-    return _.map(this.props.notes, (note, key) =>{
+    // handle change
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    // handle submit
+    handleSubmit(e) {
+        e.preventDefault();
+        const note = {
+            title: this.state.title,
+            body: this.state.body
+        };
+        this.props.saveNote(note);
+        this.setState({
+            title: '',
+            body: ''
+        });
+    }
+
+    // render notes
+    renderNotes() {
+        return _.map(this.props.notes, (note, key) => {
+            return (
+                <NoteCard key={key}>
+                    <h2>{note.title}</h2>
+                    <p>{note.body}</p>
+                    <button className="btn btn-danger btn-xs" onClick={() => this.props.deleteNote(key)}>
+                        Delete
+                    </button>
+                </NoteCard>
+            );
+        });
+    }
+
+    render() {
         return (
-          <NoteCard key={key}>
-              <h2>{note.title}</h2>
-              <p>{note.body}</p>
-              <button 
-                className="btn btn-danger btn-xs" 
-                onClick={()=>this.props.deleteNote(key)}>
-                  delete
-              </button>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-sm-6 col-sm-offset-3">
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <input
+                                    onChange={this.handleChange}
+                                    value={this.state.title}
+                                    type="text"
+                                    name="title"
+                                    className="form-control no-border"
+                                    placeholder="Title..."
+                                    required
+                                />
+                            </div>
 
-          </NoteCard>
-        )
-    });
-  }
+                            <div className="form-group">
+                                <textarea
+                                    onChange={this.handleChange}
+                                    value={this.state.body}
+                                    type="text"
+                                    name="body"
+                                    className="form-control no-border"
+                                    placeholder="Body..."
+                                    required
+                                />
+                            </div>
 
-  render() {
-    return (
-      <div className="container-fluid">
-          <div clasName="row">
-              <div className="col-sm-6 col-sm-offset-3">
-                <form onSubmit={this.handleSubmit}>
-                  <div className="form-group">
-                    <input
-                      onChange={this.handleChange}
-                      value={this.state.title}
-                      type="text" 
-                      name="title" 
-                      className="form-control no-border" 
-                      placeholder="Title.." 
-                      required 
-                      /> 
-                  </div>
-                  <div className="form-group">
-                    <textarea 
-                      onChange={this.handleChange}
-                      value={this.state.body}
-                      type="text" 
-                      name="body" 
-                      className="form-control no-border" 
-                      placeholder="Body.." 
-                      required 
-                      /> 
-                  </div>
-                  <div className="form-group">
-                    <button className="btn btn-primary col-sm-12">Save</button>
-                  </div>
-                </form>
-                <br/>
-                <br/>
-                <br/>
-                {this.renderNotes()}
-              </div>
-          </div>
-      </div>
-    );
-  }
+                            <div className="form-group">
+                                <button className="btn btn-primary col-sm-12">Save</button>
+                            </div>
+                        </form>
+                        <br />
+                        <br />
+                        <br />
+                        {this.renderNotes()}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
-//*mapping the property from the state
-function mapStateToProps(state, ownProps){
-  return {
-    notes: state.notes,
-    user: state.user
-  }
+function mapStateToProps(state, ownProps) {
+    return {
+        notes: state.notes,
+        user: state.user
+    };
 }
 
-//*dispatching
-// function mapDispatchToProps(){
-
-// }
-
-//mapping from the redux...{dispatching to the redux}
-
-export default connect(mapStateToProps, {getNotes, saveNote, deleteNote, getUser})(App);
+export default connect(mapStateToProps, { getNotes, saveNote, deleteNote, getUser })(App);
