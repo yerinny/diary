7beaee05fb4c88999a57b85384f926e5d21d727c
yerinny <yerinlee90@gmail.com>
2020-06-06 13:39:33 -0700

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,47 +1,54 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import {googleLogin, twitterLogin} from '../actions/userAction';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { googleLogin, twitterLogin } from "../actions/userAction";
 
 class Login extends Component {
-
     componentWillMount() {
         if (this.props.user !== null) {
-            this.props.history.push('/');
+            // console.log(this.props.history);
+            this.props.history.push("/");
         }
     }
 
     componentWillReceiveProps(nextProps) {
         if (nextProps.user !== null) {
-            nextProps.history.push('/');
+            nextProps.history.push("/");
         }
     }
 
-    render(){
+    render() {
         return (
             <div className="container-fluid">
-                <div className="row text-center">
-                    <div className="col-sm-12 jumbotron" style={{ marginTop: '-20px;'}}>
-                        <h1>
-                            DIARY - { new Date().getFullYear() } 
-                        </h1>
+                <div className="row">
+                    <div
+                        className="col-sm-12 jumbotron"
+                        style={{ marginTop: "-20px" }}
+                    >
+                        <h1>DIARY | {new Date().getFullYear()}</h1>
                         <h2>
-                            <i>Login with your favortie <b>Social Network</b> </i>
+                            <i>
+                                Login with your favourite <b>Social Network</b>{" "}
+                                to start writting!
+                            </i>
                         </h2>
                     </div>
-                    <div className="col-sm-6 text-center">
-                        <button className="btn btn-danger btn-lg" onClick={this.props.googleLogin}>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12 text-center">
+                        <button
+                            className="btn btn-danger col-sm-6"
+                            onClick={this.props.googleLogin}
+                        >
                             Login with Google
                         </button>
-                    </div>
-                        <br/>
-                    <div className="col-sm-6 text-center">
-                        <button className="btn btn-success btn-lg" onClick={this.props.twitterLogin}>
+
+                        <button className="btn btn-info col-sm-6" onClick={this.props.twitterLogin}>
                             Login with Twitter
                         </button>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
@@ -51,5 +58,7 @@ function mapStateToProps(state, onwProps) {
     };
 }
 
-
-export default connect(mapStateToProps, {googleLogin, twitterLogin})(Login);
+export default connect(
+    mapStateToProps,
+    { googleLogin, twitterLogin }
+)(Login);
