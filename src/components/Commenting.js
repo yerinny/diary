836a5e723d88c60b4457bcf1b1363e3b4bb102a5
yerinny <yerinlee90@gmail.com>
2020-06-06 13:00:23 -0700

    Create Commenting.js

---
+++ src/components/Commenting.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+//where comments are displayed 
+
+const Commenting = props => {
+    return (
+        <div>{props.children}</div>
+    )
+}
+
+
+export default Commenting;
