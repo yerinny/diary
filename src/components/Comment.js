3c52d694bb013085222b7bfe389f5282410a984e
yerinny <yerinlee90@gmail.com>
2020-06-06 13:00:20 -0700

    Update Comment.js

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { saveComment } from '../actions/notesActions'
 
 class Comment extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            commentBody: ''
+        };
+        //bind
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    handleChange(e) {
+        this.setState({
+            commentBody: e.target.value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const comment = {
+            commentBody: this.state.commentBody,
+            uid: this.props.uid
+        };
+        console.log(this.props.uid, comment);
+        this.props.saveComment(this.props.id, comment);
+        this.setState({ commentBody: '' });
+    }
+
     render(){
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <textarea
+                            onChange={this.handleChange}
+                            value= {this.state.commentBody}
                             type="text"
                             name="commentBody"
                             className="form-control no-border"
@@ -27,9 +59,9 @@ class Comment extends Component {
 
 function mapStateToProps(state, ownProps){
     return {
-        uid: state.user.id
+        uid: state.user.uid
     }
 }
 
 
-export default connect(mapStateToProps, {})(Comment);
+export default connect(mapStateToProps, { saveComment })(Comment);
