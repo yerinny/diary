c0feeedae5812f5ed7de729b268fc304bd3a8e66
yerinny <yerinlee90@gmail.com>
2020-06-08 13:58:33 -0700

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { googleLogin, twitterLogin } from "../actions/userAction";
+import { Link } from 'react-router-dom';
+import '../styles/login.css';
 
 class Login extends Component {
     componentWillMount() {
@@ -18,39 +20,57 @@ class Login extends Component {
 
     render() {
         return (
-            <div className="container-fluid">
-                <div className="row">
-                    <div
-                        className="col-sm-12 jumbotron"
-                        style={{ marginTop: "-20px" }}
-                    >
-                        <h1>DIARY | {new Date().getFullYear()}</h1>
-                        <h2>
-                            <i>
-                                Login with your favourite <b>Social Network</b>{" "}
-                                to start writting!
-                            </i>
-                        </h2>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-sm-12 text-center">
-                        <button
-                            className="btn btn-danger col-sm-6"
-                            onClick={this.props.googleLogin}
-                        >
-                            Login with Google
-                        </button>
 
-                        <button className="btn btn-info col-sm-6" onClick={this.props.twitterLogin}>
-                            Login with Twitter
-                        </button>
+        <div className="container bg">
+            <div className="row">
+                <div className="d-flex justify-content-center h-200">
+                    <div className="card center">
+                        <div className="card-header">
+                            < br />
+                                <h3>Sign in with your favorite Social Media to start writing</h3>
+                            < br />
+                                <div className="card-body">
+                                    <div className="row">
+                                    <div className="col-sm-12 text-center">
+                                        <button
+                                            className="btn btn-danger col-sm-10 social"
+                                            onClick={this.props.googleLogin}
+                                        >
+                                            Login with Google
+                                        </button>
+                                        <br />
+                                        <br />
+                                        <button 
+                                            className="btn btn-info col-sm-10 social" 
+                                            onClick={this.props.twitterLogin}
+                                        >
+                                            Login with Twitter
+                                        </button>
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <h6>
+                                            ThinkCreative is a page where you can post your creative mind and get insight of other people's opinion to either contribute, suggest, or add-on.
+                                        </h6>
+                                        <h6>
+                                            There is no sign-up, simply log-in with your existing account like Google, Twitter, or Facebook! Simple as just a click.
+                                        </h6>
+                                        <Link to=""> FAQ </Link>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+             </div>
+         </div>
+        )
     }
-}
+}   
 
 function mapStateToProps(state, onwProps) {
     return {
