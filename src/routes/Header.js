a53f5530fe874e84b3c69a629f1d3059ab341901
yerinny <yerinlee90@gmail.com>
2020-06-09 17:10:54 -0700

    Update Header.js

--- src/routes/Header.js
+++ src/routes/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getUser, logout } from '../actions/userAction';
 
