fc9dc7a7cd588623d82bd13458f03b4abb6d213f
yerinny <yerinlee90@gmail.com>
2020-06-28 10:00:38 -0700

    Update NoteCard.js

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -2,9 +2,9 @@ import React from 'react';
 import '../styles/notecard.css';
 
 
-
+// props.children passed down as a children which is shown <NoteCard> *what is between this* </NoteCard>
 const NoteCard = props => (
-    <div className="note jumbotron">
+    <div className="jumbotron">
         <div>{props.children}</div>
     </div>
 );
